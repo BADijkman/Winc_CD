b95532ac0b693222984937709b7f3ce413c549b8
Andre <ba.dijkman@icloud.com>
2023-09-13 20:25:26 +0200

    css

--- main.py
+++ main.py
@@ -4,21 +4,21 @@ from flask import Flask, render_template, redirect
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template('index.html', title="Index")
+    return render_template('index.html', title='Index')
 
 
-@app.route("/home")
+@app.route('/home')
 def redirect_home():
     return redirect('/')
 
 
-@app.route("/english premier league")
+@app.route('/english premier league')
 def english_premier_league():
-    return render_template('english premier league.html',  title='English Premier League')
+    return render_template('english premier league.html', title='English Premier League')
 
 
-@app.route("/dutch eredivisie")
+@app.route('/dutch eredivisie')
 def dutch_eredivisie():
-    return render_template("dutch eredivisie.html", title="Dutch Eredivisie")
+    return render_template('dutch eredivisie.html', title='Dutch Eredivisie')
