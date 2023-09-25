6171241e5fce5194352d55076a54c38389e4e028
Andre <ba.dijkman@icloud.com>
2023-09-25 17:11:46 +0200

    edit underscore in html

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ def redirect_home():
 
 @app.route('/english premier league')
 def english_premier_league():
-    return render_template('english premier league.html', title='English Premier League')
+    return render_template('english_premier_league.html', title='English Premier League')
 
 
 @app.route('/dutch eredivisie')
 def dutch_eredivisie():
-    return render_template('dutch eredivisie.html', title='Dutch Eredivisie')
+    return render_template('dutch_eredivisie.html', title='Dutch Eredivisie')
